19c658eadb68cb79d5cc806691f7d5cd54e7aff1
Anders Thorbeck <anders@thorbeck.no>
2023-11-08 11:19:14 +0100

    Define unary relations grid and valid_grid_dimensions

--- sudoku/sudoku.pl
+++ sudoku/sudoku.pl
@@ -1,5 +1,26 @@
 minigrid_box_width(2).
 minigrid_box_height(2).
 
+minigrid([
+  [1,2,3,4],
+  [3,4,1,2],
+  [2,3,4,1],
+  [4,1,2,3]
+]).
+
 grid_box_width(W) :- minigrid_box_width(W).
 grid_box_height(H) :- minigrid_box_height(H).
+
+grid(G) :- minigrid(G).
+
+valid_grid_dimensions(Grid) :-
+    grid_box_width(Width),
+    grid_box_height(Height),
+    Dimension is Width*Height,
+    grid(Grid),
+    length(Grid, Dimension),
+    forall(
+        member(Row, Grid),
+        length(Row, Dimension)
+    ).
+
